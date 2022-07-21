e6c82f68fda830b9a0df4c19b881c60d432cba9e
Alpha-Hotel <workmail2159@gmail.com>
2022-07-21 13:00:22 -0400

    Update sam_emitter.py

--- sam_emitter.py
+++ sam_emitter.py
@@ -23,7 +23,7 @@ class Sam_Emitter():
     def track_target(self, pos_reps): 
         '''Change altitude + azimuth based on degrees from desired angle'''
         pos_reps = json.loads(pos_reps)
-        pos_rep = [x for x in pos_reps if x["name"] == self.target][0]
+        pos_rep = [report for report in pos_reps if report["name"] == self.target][0]
         def double_angle_difference(angle1, angle2):
             diff = ( angle2 - angle1 + 180 ) % 360 - 180
             if (diff < -180):
@@ -68,6 +68,7 @@ class Sam_Emitter():
                 self.status="moving"
         else:
             self.status= "not ready"
+            self.slant_range_to_target=0
 
     def get_status(self):
         return {
@@ -83,34 +84,6 @@ class Sam_Emitter():
             "gnd_speed":self.speed,
             "range_to_target": self.slant_range_to_target
             }
-
-def change_target(updated_sam):
-    updated_sam=json.loads(updated_sam)
-    old_sam = [x for x in sam_list if x.name==updated_sam['sam_id']][0]
-    old_sam.target = updated_sam["cur_target"]
-    old_sam.threat = updated_sam['cur_threat']
-    old_sam.status="moving"
-
-if __name__ == "__main__":
-    #lat long height
-    pos_rep ={ "A01":{'latitude':40,
-        'longitude' : 36.400, 
-        'altitude': 2000}}
-    sam_list = [Sam_Emitter('Sam01', 36.002, 120.2312), Sam_Emitter('Sam02', 28.002, 110.2312)]
-    for sam in sam_list:
-        sam.track_target()
-    new_target = Sam_Emitter('Sam02', 38.002, 169.2312)
-    new_target.target="A01"
-    while True:
-        sam_json=[]
-        for sam in sam_list:
-            sam.track_target()
-            sam_json.append(sam.get_status())
-        change_target(json.dumps(new_target.get_status()))
-        print(sam_json)
-        sam_json=json.dumps(sam_json)
-        pos_rep["A01"]['altitude']-=50
-        pos_rep["A01"]['latitude']-=1.0
-        pos_rep["A01"]['longitude']+=1.0
-        time.sleep(1)
-
+    def change_target(self, sam_status):
+        self.target = sam_status["cur_target"]
+        self.threat = sam_status['cur_threat']
